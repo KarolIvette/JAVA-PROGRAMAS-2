bd632a8d7dd7570d88e01fa56f2b87f49177eed3
KarolIvette <karolcedeno95@gmail.com>
2016-06-07 10:55:48 -0500

    CLASE VECTOR MES

---
+++ MesVector/src/MesVector/NumeroMes.java
@@ -0,0 +1,26 @@
+package MesVector;
+import javax.swing.JOptionPane;
+
+import javax.swing.JOptionPane;
+
+public class NumeroMes {
+	int numes;
+
+	public NumeroMes(String[] arr) {
+		String vector[] = new String [12];
+	}
+	
+	public int get_numes(){
+		return this.numes; 
+	}
+	
+	public void set_numes(int numes){
+		this.numes =  numes ; 
+	}
+	
+	static void getNombreMes(String[] arr, int numes){
+			for(int i = numes; i<numes+1; i++){
+				JOptionPane.showMessageDialog(null, "MES: "+arr[i-1]);
+			}
+		}
+}
